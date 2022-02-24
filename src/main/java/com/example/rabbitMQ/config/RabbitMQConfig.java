a476377934b3bb4f298ddf5994cca265a0becde6
ayse.ok <ayse.ok@BID095.yok82.yerel>
2022-02-24 11:16:42 +0300

    code review

--- src/main/java/com/example/rabbitMQ/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabbitMQ/config/RabbitMQConfig.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan(basePackages = { "com.example.rabbitMQ" })
